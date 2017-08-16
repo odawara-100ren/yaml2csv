6e4a46682d6a71007094ae792d330beffef842df
odawara <speedmod@gmail.com>
2017-08-16 12:49:58 +0900

    最初の実装

--- lib/yaml2csv.rb
+++ lib/yaml2csv.rb
@@ -1,5 +1,78 @@
 require "yaml2csv/version"
+require 'yaml'
+require 'csv'
+require 'pry'
 
 module Yaml2csv
-  # Your code goes here...
+  class Yaml2csv
+    @@depth = 0
+    @@array = []
+
+    # @param [String] file Yamlのファイル名
+    def self.yaml_to_csv(path)
+      hash = YAML::load_file(path)
+      depth(hash, 0)
+      row_num = end_node_size(hash)
+      @@array = Array.new(@@depth).map{ Array.new(row_num) }
+      walk(hash, 0, 0)
+      p @@array
+      # @@array.each do |col|
+      # end
+    end
+
+    # @param [Object] v walk対象
+    # @param [Fixnum] depth 深さ
+    # @param [Fixnum] row_num 行番号
+    # @@array[depth][row_num]に新規に書き込む形になる。
+    def self.walk(v, depth, row_num)
+      # @@array[depth] ||= []
+      case v
+      when Hash
+        cursor = row_num
+        v.each do |key, val|
+          # ひとつのkeyについて以下の作業
+          walk(val, depth+1, cursor)
+          row_size = end_node_size(val)
+          # walkとend_node_sizeをまとめればよさそう。
+          # 今のところは分けておく
+          row_size.times do |i|
+            @@array[depth][cursor] = key
+            cursor += 1
+          end
+        end
+      when Array
+        v.each.with_index do |val, i|
+          @@array[depth][row_num + i] = val
+        end
+      else
+        @@array[depth][row_num] = v
+      end
+    end
+
+    def self.end_node_size(v)
+      case v
+      when Hash
+        size = 0
+        v.values.each do |val|
+          size += end_node_size(val)
+        end
+        size
+      when Array
+        v.size
+      else
+        1
+      end
+    end
+
+    def self.depth(v, depth)
+      case v
+      when Hash
+        v.values.each {|val| depth(val, depth + 1)}
+      when Array
+        @@depth = depth+2 if @@depth < depth+2
+      else
+        @@depth = depth+1 if @@depth < depth+1
+      end
+    end
+  end
 end
--- yaml2csv.gemspec
+++ yaml2csv.gemspec
@@ -9,8 +9,8 @@ Gem::Specification.new do |spec|
   spec.authors       = ["odawara"]
   spec.email         = ["speedmod@gmail.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
+  spec.summary       = "yaml to csv" #%q{TODO: Write a short summary, because Rubygems requires one.}
+  spec.description   = "yaml to csv" #%q{TODO: Write a longer description or delete this line.}
   spec.homepage      = "TODO: Put your gem's website or public repo URL here."
   spec.license       = "MIT"
 
@@ -33,4 +33,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.15"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency "pry-byebug"
+  spec.add_dependency "thor"
 end
