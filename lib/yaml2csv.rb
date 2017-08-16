cdb5e0d80462acbcfd563b15cad39353950695dd
odawara <speedmod@gmail.com>
2017-08-16 14:02:07 +0900

    walkをリファクタして、end_node_sizeメソッドを逐一呼ばなくてもいいようにした

--- lib/yaml2csv.rb
+++ lib/yaml2csv.rb
@@ -34,31 +34,34 @@ module Yaml2csv
     # @param [Fixnum] depth 深さ
     # @param [Fixnum] row_num 行番号
     # @@array[depth][row_num]に新規に書き込む形になる。
+    # @return [Fixnum] walk対象が持つ末端ノードの数
     def self.walk(v, depth, row_num)
-      # @@array[depth] ||= []
       case v
       when Hash
+        end_node_size = 0
         cursor = row_num
         v.each do |key, val|
-          # ひとつのkeyについて以下の作業
-          walk(val, depth+1, cursor)
-          row_size = end_node_size(val)
-          # walkとend_node_sizeをまとめればよさそう。
-          # 今のところは分けておく
+          row_size = walk(val, depth+1, cursor)
           row_size.times do |i|
             @@array[depth][cursor] = key
             cursor += 1
           end
+          end_node_size += row_size
         end
       when Array
         v.each.with_index do |val, i|
           @@array[depth][row_num + i] = val
         end
+        end_node_size = v.size
       else
         @@array[depth][row_num] = v
+        end_node_size = 1
       end
+      end_node_size
     end
 
+    # @param [Object] v walk対象
+    # @return [Fixnum] walk対象が持つ末端ノードの数
     def self.end_node_size(v)
       case v
       when Hash
@@ -74,6 +77,9 @@ module Yaml2csv
       end
     end
 
+    # クラス変数@@depth（対象のハッシュの階層の深さ）を初期化する
+    # @param [Object] v walk対象
+    # @param [Fixnum] depth 深さを表す数値。ルートが0
     def self.depth(v, depth)
       case v
       when Hash
